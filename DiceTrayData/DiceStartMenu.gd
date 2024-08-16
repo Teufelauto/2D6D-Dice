236da36274f552f49c6e4cc6c1e5cb4ae5dea829
Teufelauto <110637430+Teufelauto@users.noreply.github.com>
2024-08-15 21:27:17 -0400

    Restructured files

---
+++ DiceTrayData/DiceStartMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://dice_world.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://DiceTrayData/dice_options_menu.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
---
+++ DiceTrayData/dice_options_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_change_colors_pressed():
+	pass # Replace with function body.
+
+
+func _on_dice_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://DiceTrayData/dice_start_menu.tscn")
