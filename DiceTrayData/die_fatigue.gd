39cb9329ef9b3932c44c620e8f1f2f3459df6de9
Teufelauto <110637430+Teufelauto@users.noreply.github.com>
2024-09-17 20:11:16 -0400

    Fatigue die enhancement (#17)
    * added combat diefunctions
    
    * Combat Mode now works

--- DiceTrayData/die_fatigue.gd
+++ DiceTrayData/die_fatigue.gd
@@ -3,14 +3,15 @@ class_name FatigueDie
 
 @export var battle_round :int = 1
 @export var fatigue_die_angle :float = 2.692794 ## [Radians] set for displaying 1. 
-
+@export var fatigue_die_in_combat_state :bool
 
 
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	freeze = true
 	return_fatigue_die_to_one()
-	
+	fatigue_die_in_combat_state = false
+	position_for_exploration()
 
 ## when signaled to increment 1
 func increment_fatigue_die_to_next_round() -> void:
@@ -36,3 +37,38 @@ func increment_fatigue_die_to_next_round() -> void:
 func return_fatigue_die_to_one() -> void:
 	battle_round = 0
 	increment_fatigue_die_to_next_round()
+
+
+func position_determination_on_click() -> void:
+	#print("fatigue_die_in_combat_state is " + str(fatigue_die_in_combat_state))
+	if fatigue_die_in_combat_state: ## In Combat before button pressed to switch to exploration.
+		position_for_exploration()
+		#print("Now Exploring")
+		fatigue_die_in_combat_state = false ## Make opposite
+		$"../../DiceCanvas/CombatSelectButton".text = "Begin Combat" ## New button text
+		
+	else: ## in Exploration before button pressed to change to combat.
+		position_for_combat()
+		#print("In Mortal Combat!")
+		fatigue_die_in_combat_state = true ## Make opposite
+		$"../../DiceCanvas/CombatSelectButton".text = "End Combat" ## New button text.
+
+
+## Tip up the roller on its end.
+func position_for_exploration() -> void:
+	rotation_degrees.x = -90
+	position.x = -0.03
+	position.y = 0.145
+	position.z = -0.14
+	$"../../DiceCanvas/FatigueIncrementButton".visible = false
+	$"../StaticBody3DFatigue".set_collision_layer_value( 3, false) ## Sloped Collision box
+
+
+## Lay the roller down on its side.
+func position_for_combat() -> void:
+	rotation_degrees.x = 0
+	position.x = 0
+	position.y = 0
+	position.z = 0
+	$"../../DiceCanvas/FatigueIncrementButton".visible = true
+	$"../StaticBody3DFatigue".set_collision_layer_value( 3, true) ## Sloped Collision box
